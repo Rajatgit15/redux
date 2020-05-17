c205dd3faa49cf8761aab5f800fc7505930b7b27
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-05-17 21:13:43 +0530

    Updates

---
+++ precache-manifest.2ce479f8cf95725b72d35aafeac6a7a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7191737761de8be1c89fd7a194a938ce",
+    "url": "/redux/index.html"
+  },
+  {
+    "revision": "5266c8d8645dae8ba2b0",
+    "url": "/redux/static/css/main.9a80f2f7.chunk.css"
+  },
+  {
+    "revision": "106c7ac083c3350f99cc",
+    "url": "/redux/static/js/2.8f0d7831.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux/static/js/2.8f0d7831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5266c8d8645dae8ba2b0",
+    "url": "/redux/static/js/main.7a79a281.chunk.js"
+  },
+  {
+    "revision": "9d3eeb2c468c71df9b72",
+    "url": "/redux/static/js/runtime-main.78f94f71.js"
+  }
+]);
